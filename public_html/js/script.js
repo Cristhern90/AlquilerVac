b2c1f3910fc2eb9ddd8fef669a3ee46dd86f8ccd
Cristian Hernan de la Rosa <cristhern20@gmail.com>
2022-05-12 16:25:30 +0200

    ver 0.01

---
+++ public_html/js/script.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function () {
+    $('.slider').slick({
+        infinite: true, 
+        speed: 500, 
+        slidesToShow: 1, 
+        slidesToScroll: 1, 
+        dots: true, 
+        autoplay: true, 
+        autoplaySpeed: 5000, 
+        fade: true
+    });
+});
