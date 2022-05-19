14b350e73dd75c9c21491c0515290ed2164bbb56
Cristian Hernan de la Rosa <cristhern20@gmail.com>
2022-05-19 17:29:46 +0200

    ver 0.03
    charge data from json

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -1,13 +1,40 @@
 
 $(document).ready(function () {
-    $('.slider').slick({
-        infinite: true, 
-        speed: 500, 
-        slidesToShow: 1, 
-        slidesToScroll: 1, 
-        dots: true, 
-        autoplay: true, 
-        autoplaySpeed: 5000, 
-        fade: true
+
+    $.getJSON("datos/slider.json", function (data) {
+        var items = [];
+        $.each(data, function (key, val) {
+            var item_html = "";
+            item_html += '<div class="item_slider">';
+            item_html += '<div class="item_slider_content text-end" style="background-image: url(images/' + val.img + ')">';
+            item_html += '<div class="oscur position-relative p-4">';
+            item_html += '<h2>' + val.title + '</h2> ' + val.descript;
+            item_html += '</div>';
+            item_html += '</div>';
+
+            $(".slider").append(item_html);
+        });
+        $('.slider').slick({
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: true,
+            autoplay: true,
+            autoplaySpeed: 5000,
+            fade: true
+        });
     });
+
+
+//    $('.slider').slick({
+//        infinite: true,
+//        speed: 500,
+//        slidesToShow: 1,
+//        slidesToScroll: 1,
+//        dots: true,
+//        autoplay: true,
+//        autoplaySpeed: 5000,
+//        fade: true
+//    });
 });
